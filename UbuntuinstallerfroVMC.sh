d05e9ecead121974f60a58eec360211225880249
Franceschinie <94822523+Franceschinie@users.noreply.github.com>
2023-07-16 18:24:43 +0200

    Update UbuntuinstallerfroVMC.sh

--- UbuntuinstallerfroVMC.sh
+++ UbuntuinstallerfroVMC.sh
@@ -1,5 +1,8 @@
 #!/bin/sh 
+echo "stop the machine"
 lxc stop -f penguin
 lxc delete penguin
+echo "download and install Ubuntu 23.04"
 lxc launch ubuntu:23.04 penguin --debug
+echo "starting setup"
 lxc exec penguin -- bash <(curl https://raw.githubusercontent.com/Franceschinie/switch-your-ChromeOS-Linux-from-Debian-to-Ubuntu/main/setup.sh)
