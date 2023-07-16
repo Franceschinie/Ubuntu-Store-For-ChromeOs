1fbf6f1aa2ba738ee6df42a1bf0c3a0a52b09897
Franceschinie <94822523+Franceschinie@users.noreply.github.com>
2023-07-16 19:14:30 +0200

    Update UbuntuinstallerfroVMC.sh

--- UbuntuinstallerfroVMC.sh
+++ UbuntuinstallerfroVMC.sh
@@ -1,5 +1,5 @@
 #!/bin/sh 
 lxc stop -f penguin
 lxc delete penguin
-lxc launch ubuntu:23.04 penguin --debug
+lxc launch ubuntu:23.04 penguin --debug < /dev/null
 lxc exec penguin -- curl -s https://raw.githubusercontent.com/Franceschinie/switch-your-ChromeOS-Linux-from-Debian-to-Ubuntu/main/setup.sh | bash -s 
